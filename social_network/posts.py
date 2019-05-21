377bf36bb17269f29eea2af61ebd578ef39804cf
YuchuanWeng <cts734@gmail.com>
2019-05-21 12:12:46 +0000

    modify super class syntax

--- social_network/posts.py
+++ social_network/posts.py
@@ -15,7 +15,7 @@ class Post(object):
 
 class TextPost(Post):  # Inherit properly
     def __init__(self, text, timestamp=None):
-        super().__init__(text, timestamp)
+        super(TextPost, self).__init__(text, timestamp)
 
     def __str__(self):
         return '@{} {}: "{}"\n\t{}'.format(self.user.first_name, self.user.last_name, self.text, self.timestamp.strftime("%A, %b %d, %Y"))
@@ -23,7 +23,7 @@ class TextPost(Post):  # Inherit properly
 
 class PicturePost(Post):  # Inherit properly
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(text, timestamp)
+        super(PicturePost, self).__init__(text, timestamp)
         self.image_url = image_url
 
     def __str__(self):
@@ -32,7 +32,7 @@ class PicturePost(Post):  # Inherit properly
 
 class CheckInPost(Post):  # Inherit properly
     def __init__(self, text, latitude, longitude, timestamp=None):
-        super().__init__(text, timestamp)
+        super(CheckInPost, self).__init__(text, timestamp)
         self.latitude = latitude
         self.longitude = longitude
         
